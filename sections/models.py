cc836d39db2ce5442b0057931e80353037b8ff76
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2022-09-01 18:23:47 -0400

    add meeting model

--- sections/models.py
+++ sections/models.py
@@ -4,9 +4,39 @@ from courses.models import Course
 from universities.models import University
 from django.urls import reverse
 from django.db.models import Q
+from datetime import datetime
 import re
 
 # Create your models here.
+class Meeting(models.Model):
+    day = models.CharField(
+        max_length=10,
+        )
+    meeting_type = models.CharField(
+        max_length=50,
+        choices=[
+            ('L', 'Lab'),
+            ('T', 'Lecture'),
+            ('P', 'Recitation'),
+            ('W', 'Workshop'),
+            ('O', 'Other'),
+            ],
+            blank=True,
+        )
+
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    location = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True)
+
+    class Meta:
+        ordering = ['day', 'start_time']
+
+    def __str__(self):
+        return f"{self.day} {self.start_time} - {self.end_time} ({self.location})"
+
 
 class Section(models.Model):
     name = models.CharField(
@@ -24,6 +54,10 @@ class Section(models.Model):
         blank=True,
         related_name='sections')
 
+    meetings = models.ManyToManyField(
+        Meeting,
+        related_name='sections')
+
     canvas_id = models.CharField(
         max_length=100,
         null=True,
@@ -39,13 +73,57 @@ class Section(models.Model):
         return reverse('sections:detail', kwargs={'pk': self.pk})
 
     @classmethod
+    def update_from_ufsoc(cls, requester, course):
+        user = User.objects.get(username=requester)
+        from autocanvas.pipes.ufsoc import get_course_from_UFSOC_apix
+        json_response = get_course_from_UFSOC_apix(
+            course.term,
+            course_code=course.course_code,
+            instructor_name=user.last_name,
+            program_level_name="Undergraduate",
+        )
+        course.description = json_response[0]["description"]
+        course.save()
+        ufsoc_sections = json_response[0]["sections"]
+        if not ufsoc_sections:
+            print(f"No sections found in UF SOC. Skipping ...")
+            return []
+        for ufsoc_section in ufsoc_sections:
+            assert len(str(ufsoc_section['classNumber'])) == 5
+            section = cls.objects.filter(name__contains=f"({ufsoc_section['classNumber']})").first()
+
+            if section:
+                print(f"UF SOC section {ufsoc_section['classNumber']} matches course section {section.name}. Updating ...")
+                section.meetings.remove()
+                for ufsoc_meeting in ufsoc_section['meetTimes']:
+                    start_time = datetime.strptime(
+                        ufsoc_meeting['meetTimeBegin'], 
+                        "%I:%M %p")
+                    end_time = datetime.strptime(
+                        ufsoc_meeting['meetTimeEnd'], 
+                        "%I:%M %p")
+                    meeting, _ = Meeting.objects.get_or_create(
+                        day="".join(ufsoc_meeting['meetDays']),
+                        start_time=start_time,
+                        end_time=end_time,
+                        location=f"{ufsoc_meeting['meetBuilding']} {ufsoc_meeting['meetRoom']}",)
+                    section.meetings.add(meeting)
+            else:
+                print(f"Could not find UF SOC section {ufsoc_section['classNumber']} in course sections. Skipping.")
+                continue
+            section.save()
+
+        return
+
+    
+    @classmethod
     def update_from_canvas(cls, requester, course, canvas_sections):
         user = User.objects.get(username=requester)
         for canvas_section in canvas_sections:
             section = cls.objects.filter(
                 Q(canvas_id=canvas_section['id'])
                 | Q(name=canvas_section['name'])
-                | Q(name=re.match(".*\((\d{5})\).*",canvas_section['name']).group(1))
+                | Q(name__contains="(canvas_section['name'])")
                 ).first()
 
             if section:
@@ -61,7 +139,12 @@ class Section(models.Model):
                     course=course)
             if canvas_section['enrollment_role']=='TaEnrollment':
                 section.teaching_assistant = user
+            
             section.save()
 
+        Section.update_from_ufsoc(requester, course)
             
         return cls.objects.filter(course=course)
+
+    
+
