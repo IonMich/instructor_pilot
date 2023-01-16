31fd65586c590a36608f69bbfb4b824f3accffe4
Sanjib Katuwal <katsanjib046@gmail.com>
2023-01-16 18:12:44 -0500

    Changes made to assignments/urls.py, views.py and submissions/models.py. Added a single clustering field to PaperSubmissionImage Added cluster-view function in views.py to run clustering Made corresponding changes to urls.py

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import assignment_detail_view
+from .views import assignment_detail_view, cluster_view
 
 app_name = 'assignments'
 
 urlpatterns = [
     path('courses/<course_pk>/assignments/<assignment_pk>/', assignment_detail_view, name='detail'),
+    path('courses/<course_pk>/assignments/<assignment_pk>/cluster/', cluster_view, name='cluster'),
 ]
