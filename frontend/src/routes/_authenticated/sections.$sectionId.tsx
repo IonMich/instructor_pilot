6c3ebb62e4ae84d56bd83d0dfaf725305acbfe21
Ioannis Michaloliakos <ioannis.michalol@ufl.edu>
2024-04-23 17:34:06 -0400

    add section detail route and UI

---
+++ frontend/src/routes/_authenticated/sections.$sectionId.tsx
@@ -0,0 +1,49 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { sectionQueryOptions, studentsOfSectionQueryOptions } from '@/utils/queryOptions'
+import { StudentDeck } from '@/routes/-components/sectionDetailLists'
+import { Course, Section } from '@/utils/fetchData';
+
+function getBreadcrumbItems(course: Course, section: Section) {
+  return [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: course.course_code,
+      path: `/courses/${course.id}`,
+    },
+    {
+      title: `${section.name}` ?? `Section ${section.id}` ?? 'Section',
+      path: `/sections/${section.id}`,
+    },
+  ];
+}
+
+export const Route = createFileRoute('/_authenticated/sections/$sectionId')({
+  parseParams: (params) => ({
+    sectionId: parseInt(params.sectionId),
+  }),
+  stringifyParams: (params) => ({
+    sectionId: params.sectionId.toString(),
+  }),
+  loader: async (opts) => {
+    const sectionId = opts.params.sectionId
+    const sectionPromise = opts.context.queryClient.ensureQueryData(
+      sectionQueryOptions(sectionId)
+    )
+    const studentsPromise = opts.context.queryClient.ensureQueryData(
+      studentsOfSectionQueryOptions(sectionId)
+    )
+    const [section, students] = await Promise.all([sectionPromise, studentsPromise])
+    const course = section.course
+    return {
+      section: section,
+      course: course,
+      students: students,
+      title: `Section ${sectionId}`,
+      breadcrumbItems: getBreadcrumbItems(course, section),
+    }
+  },
+  component: StudentDeck
+})
